b8c0316ac94a91353ef3679dc5e26836d135298a
Kalpit Shah <kalpit.fxdatalabs@gmail.com>
2020-04-29 15:34:27 +0530

    added the logic of calling from index to logger || separated from index

--- index.js
+++ index.js
@@ -1,9 +1,3 @@
-const Logger = require("./logger");
 
-const logger = new Logger();
-logger.on("message", data => console.log("Called Listener ", data));
 
-logger.log("Hello World");
-logger.log("Hello World New");
-logger.log("Hello World What");
-logger.log("Hello World New1");
+
