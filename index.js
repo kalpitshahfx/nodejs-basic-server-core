d1df5d03793ff8bbfda81e11463a51ea7f8405ac
Kalpit Shah <kalpit.fxdatalabs@gmail.com>
2020-04-29 16:36:10 +0530

    added the relevant css and used core modules to serve the html files in index.js

--- index.js
+++ index.js
@@ -3,25 +3,81 @@ const path = require("path");
 const fs = require("fs");
 
 const server = http.createServer((req, res) => {
-  if (req.url === "/") {
-    fs.readFile(
-      path.join(__dirname, "public", "index.html"),
-      (err, content) => {
-        if (err) throw err;
-        res.writeHead(200, { "Content-Type": "text/html" });
-        res.end(content);
-      }
-    );
+  //   if (req.url === "/") {
+  //     fs.readFile(
+  //       path.join(__dirname, "public", "index.html"),
+  //       (err, content) => {
+  //         if (err) throw err;
+  //         res.writeHead(200, { "Content-Type": "text/html" });
+  //         res.end(content);
+  //       }
+  //     );
+  //   }
+  //   if (req.url === "/api/users") {
+  //     const users = [
+  //       { name: "SIRI", age: 25 },
+  //       { name: "Alexa", age: 100 }
+  //     ];
+  //     res.writeHead(200, { "Content-Type": "application/json" });
+  //     res.end(JSON.stringify(users));
+  //   }
+
+  //Build File Path
+
+  let filePath = path.join(
+    __dirname,
+    "public",
+    req.url === "/" ? "index.html" : req.url
+  );
+
+  // Extension of file
+  let extName = path.extname(filePath);
+
+  //Initial Content Type
+  let contentType = "text/html";
+
+  //check ext and set content type
+  switch (extName) {
+    case ".js":
+      contentType = "text/javascript";
+      break;
+    case ".css":
+      contentType = "text/css";
+      break;
+    case ".json":
+      contentType = "application/json";
+      break;
+    case ".png":
+      contentType = "image/png";
+      break;
+    case ".jpg":
+      contentType = "image/jpg";
+      break;
   }
 
-  if (req.url === "/api/users") {
-    const users = [
-      { name: "SIRI", age: 25 },
-      { name: "Alexa", age: 100 }
-    ];
-    res.writeHead(200, { "Content-Type": "application/json" });
-    res.end(JSON.stringify(users));
-  }
+  // Read file
+  fs.readFile(filePath, (err, content) => {
+    if (err) {
+      if (err.code === "ENOENT") {
+        // Page Not Found
+        fs.readFile(
+          path.join(__dirname, "public", "404.html"),
+          (err, content) => {
+            res.writeHead(200, { "Content-Type": "text/html" });
+            res.end(content, "utf-8");
+          }
+        );
+      } else {
+        // Some server Error
+        res.writeHead(500);
+        res.end(`Server Error ${err.code}`);
+      }
+    } else {
+      // Success
+      res.writeHead(200, { "Content-Type": contentType });
+      res.end(content, "utf-8");
+    }
+  });
 });
 
 const PORT = process.env.PORT || 3000;
